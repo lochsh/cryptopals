996f22a9722dd74b5a1936d3d16f67512f5328fe
Hannah McLaughlin <h@mcla.ug>
2016-11-12 22:31:53 +0000

    now free result to prevent memory leak

--- challenges_1/test_hex_to_base64.c
+++ challenges_1/test_hex_to_base64.c
@@ -6,6 +6,7 @@ static char* test_hex_str_to_bytes() {
     uint8_t expected[] = {0xde, 0xad, 0xbe, 0xbf};
     uint8_t *result = hex_str_to_bytes("deadbeef");
     mu_assert("error deadbeef!", result == expected);
+	free(result);
     return 0;
 }
 
