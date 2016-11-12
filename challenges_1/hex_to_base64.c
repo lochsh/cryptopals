9547848a7e466cabc67cfc3345c2de36a6bb93e2
Hannah McLaughlin <h@mcla.ug>
2016-11-12 23:10:20 +0000

    accidentally mixed tabs and spaces after copying from some other code I wrote a long time ago!  fixed now

--- challenges_1/hex_to_base64.c
+++ challenges_1/hex_to_base64.c
@@ -8,10 +8,10 @@ uint8_t* hex_str_to_bytes(char* hex_str) {
     int num_bytes = strlen(hex_str) / 2;
 
     uint8_t *bytes = (uint8_t *) malloc(num_bytes);
-	if (bytes == NULL) {
-		perror("Error allocating memory");
-		return NULL;
-	}
+    if (bytes == NULL) {
+        perror("Error allocating memory");
+        return NULL;
+    }
 
     size_t i;
     for (i = 0; i < num_bytes; i++) {
