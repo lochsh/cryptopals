8ed7e74420fc7f609f8118e6e8798dcf409bc497
Hannah McLaughlin <h@mcla.ug>
2016-11-13 18:16:34 +0000

    reimplemented hex string to bytes, do it myself rather than using sscanf.  much more fun and interesting!

--- challenges_1/hex_to_base64.c
+++ challenges_1/hex_to_base64.c
@@ -1,21 +1,37 @@
 #include <errno.h>
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-uint8_t* hex_str_to_bytes(char* hex_str) {
-    int num_bytes = strlen(hex_str) / 2;
 
+uint8_t hex_char_to_padded_byte(char hex_char) {
+    const uint8_t mask = 15;
+    const uint32_t boundary = 64;
+
+    return (hex_char & mask) + floor(hex_char % boundary) * 9;
+}
+
+
+uint8_t hex_char_pair_to_byte(char hex_ms, char hex_ls) {
+    return (hex_char_to_padded_byte(hex_ms) << 4) + \
+            hex_char_to_padded_byte(hex_ls);
+}
+
+
+uint8_t* hex_str_to_bytes(char* hex_str) {
+    size_t i;
+    int num_bytes = strlen(hex_str) / 2;
     uint8_t *bytes = (uint8_t *) malloc(num_bytes);
+
     if (bytes == NULL) {
         perror("Error allocating memory");
         return NULL;
     }
 
-    size_t i;
     for (i = 0; i < num_bytes; i++) {
-        sscanf(&hex_str[2*i], "%02hhx", &bytes[i]);
+        bytes[i] = hex_char_pair_to_byte(hex_str[i*2], hex_str[i*2 + 1]);
     }
 
     return bytes;
