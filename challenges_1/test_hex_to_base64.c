a8db49e9c32fe80c4b1e4f48e78ba077849b450e
Hannah McLaughlin <h@mcla.ug>
2016-11-13 20:34:21 +0000

    having irritating problems with testing framework, have put in cruddy manual tests for now

--- challenges_1/test_hex_to_base64.c
+++ challenges_1/test_hex_to_base64.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
-#include <check.h>
+#include <stdint.h>
 #include "hex_to_base64.c"
 
 
-START_TEST(test_hex_str_to_bytes)
-{
-    uint8_t expected[] = {0xde, 0xad, 0xbe, 0xbf};
-    uint8_t *result = hex_str_to_bytes("deadbeef");
-    ck_assert(result == expected);
-    free(result);
+void test_hex_char_to_padded_byte() {
+    char hex_char = 'd';
+    uint8_t expected = 13;
+    uint8_t result = hex_char_to_padded_byte(hex_char);
+    if (expected == result) {
+        printf("Test hex char to padded byte passes\n");
+    }
 }
-END_TEST
 
 
-int main(void) {
+void test_hex_char_pair_to_byte() {
+    uint8_t expected = 222;
+    uint8_t result = hex_char_pair_to_byte('d', 'e');
+    if (expected == result) {
+        printf("Test hex char pair to byte passes\n");
+    }
+}
+
+
+void test_hex_str_to_bytes() {
+    uint8_t expected[] = {0xde, 0xad, 0xbe, 0xef};
+    uint8_t* result = hex_str_to_bytes("deadbeef");
+    int i;
+    int fail = 0;
+
+    for (i = 0; i < 4; i++) {
+        if (expected[i] != result[i]) {
+            fail = 1;
+        }
+    }
+    if (fail == 0) {
+        printf("Test hex str to bytes passes\n");
+    }
+}
+
+
+int main() {
+    test_hex_char_to_padded_byte();
+    test_hex_char_pair_to_byte();
+    test_hex_str_to_bytes();
     return 0;
 }
