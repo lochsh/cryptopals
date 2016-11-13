7f86e5f2df254d7355dfc3afffc2123ed8743b97
Hannah McLaughlin <h@mcla.ug>
2016-11-13 18:38:49 +0000

    now using check testing framework

--- challenges_1/test_hex_to_base64.c
+++ challenges_1/test_hex_to_base64.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-#include "../minunit.h"
+#include <check.h>
 #include "hex_to_base64.c"
 
-static char* test_hex_str_to_bytes() {
+
+START_TEST(test_hex_str_to_bytes)
+{
     uint8_t expected[] = {0xde, 0xad, 0xbe, 0xbf};
     uint8_t *result = hex_str_to_bytes("deadbeef");
-    mu_assert("error deadbeef!", result == expected);
+    ck_assert(result == expected);
     free(result);
+}
+END_TEST
+
+int main(void) {
     return 0;
 }
-
-int main() {
-    char *result = test_hex_str_to_bytes();
-    return result != 0;
- }
