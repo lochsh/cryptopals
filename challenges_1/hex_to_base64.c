06ade408fa21c5e4a3b81002aec1d939a32db14a
Hannah McLaughlin <h@mcla.ug>
2016-11-15 22:10:22 +0000

    added function for making 6 bit chunks from byte array, in prep for base64 encoding. currently only works for byte arrays of length divisible by 3

--- challenges_1/hex_to_base64.c
+++ challenges_1/hex_to_base64.c
@@ -1,24 +1,31 @@
 #include <errno.h>
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MASK_4_LSB 0x0F
+#define ASCII_NUM_LETTER_BOUND 64
 
-uint8_t hex_char_to_padded_byte(char hex_char) {
-    const uint8_t mask = 15;
-    const uint32_t boundary = 64;
-    return (hex_char & mask) + (hex_char / boundary) * 9;
+
+uint32_t offset_mask(const uint8_t num_bits, const uint8_t start_bit) {
+    return ((uint32_t) pow(2, num_bits) - 1) << (32 - start_bit - num_bits);
 }
 
 
-uint8_t hex_char_pair_to_byte(char hex_ms, char hex_ls) {
-    return (hex_char_to_padded_byte(hex_ms) << 4) + \
+uint8_t hex_char_to_padded_byte(const char hex_char) {
+    return (hex_char & MASK_4_LSB) + (hex_char / ASCII_NUM_LETTER_BOUND) * 9;
+}
+
+
+uint8_t hex_char_pair_to_byte(const char hex_ms, const char hex_ls) {
+    return (hex_char_to_padded_byte(hex_ms) << 4) +
             hex_char_to_padded_byte(hex_ls);
 }
 
 
-uint8_t* hex_str_to_bytes(char* hex_str) {
+uint8_t* hex_str_to_bytes(const char* const hex_str) {
     const int num_bytes = strlen(hex_str) / 2;
     uint8_t *bytes = (uint8_t *) malloc(num_bytes);
 
@@ -32,4 +39,27 @@ uint8_t* hex_str_to_bytes(char* hex_str) {
     }
 
     return bytes;
+}
+
+
+uint8_t* six_bit_chunks(const uint8_t* const bytes, const uint8_t num_bytes) {
+    const int num_chunks = 8 * num_bytes / 6;
+    uint8_t* chunks = (uint8_t*) malloc(num_chunks);
+
+    if (chunks == NULL) {
+        perror("Error allocating memory");
+        return NULL;
+    }
+
+    for (int i = 0; i < num_chunks - 1; i += 3) {
+        uint32_t x = ((uint32_t) bytes[i] << 24) +
+                     ((uint32_t) bytes[i+1] << 16) +
+                     ((uint32_t) bytes[i+2] << 8);
+
+        for (int j = 0; j < i + 4; j++) {
+            chunks[j] = (x & offset_mask(6, j*6)) >> (32 - (j + 1)*6);
+        }
+    }
+
+    return chunks;
 }
--- challenges_1/test_hex_to_base64.cpp
+++ challenges_1/test_hex_to_base64.cpp
@@ -5,7 +5,7 @@
 #include "catch.hpp"
 
 
-TEST_CASE( "Hex char is converted to padded byte", "[hex]" ) {
+TEST_CASE( "Hex char is converted to padded byte" ) {
     const char l_letters[] = {'a', 'b', 'c', 'd', 'e', 'f'};
     const char u_letters[] = {'A', 'B', 'C', 'D', 'E', 'F'};
 
@@ -20,13 +20,48 @@ TEST_CASE( "Hex char is converted to padded byte", "[hex]" ) {
 }
 
 
-TEST_CASE( "Hex char pair is converted to byte", "[hex]" ) {
+TEST_CASE( "Hex char pair is converted to byte" ) {
     REQUIRE( hex_char_pair_to_byte('d', 'e') == 222 );
 }
 
 
-TEST_CASE( "Hex string is converted to bytes", "[hex]" ) {
+TEST_CASE( "Hex string is converted to bytes") {
     const uint8_t expected[] = {0xde, 0xad, 0xbe, 0xef};
     REQUIRE( memcmp(hex_str_to_bytes((char*)"deadbeef"), expected,
                     sizeof(expected)) == 0 );
+}
+
+
+TEST_CASE( "Offset mask returns appropriate masks" ) {
+    REQUIRE( offset_mask(0, 0) == 0 );
+    REQUIRE( offset_mask(6, 0) == 4227858432 );
+    REQUIRE( offset_mask(1, 31) == 1 );
+}
+
+
+TEST_CASE( "Dividing all zero bytes into 6 bit chunks" ) {
+    const int num_bytes = 3;
+    const int num_chunks = 8 * num_bytes / 6;
+
+    uint8_t bytes[num_bytes] = {0};
+    const uint8_t chunks[num_chunks] = {0};
+
+    uint8_t* result = six_bit_chunks(bytes, num_bytes);
+    for (int i = 0; i < num_chunks; i++) {
+        REQUIRE( result[i] == chunks[i] );
+    }
+}
+
+
+TEST_CASE( "Dividing bytes into 6 bit chunks" ) {
+    const int num_bytes = 3;
+    const int num_chunks = 8 * num_bytes / 6;
+
+    uint8_t bytes[] = {0, 1, 2};
+    const uint8_t chunks[] = {0, 0, 4, 2};
+
+    uint8_t* result = six_bit_chunks(bytes, num_bytes);
+    for (int i = 0; i < num_chunks; i++) {
+        REQUIRE( result[i] == chunks[i] );
+    }
 }
