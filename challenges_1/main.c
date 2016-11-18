d7611076c768520b6efe147031a7d62b3ba64e84
Hannah McLaughlin <h@mcla.ug>
2016-11-18 20:28:42 +0000

    Updated main

--- challenges_1/main.c
+++ challenges_1/main.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("\x1B[31m Please provide one hex encoded string\n");
     } else {
-        uint8_t* base_64 = hex_to_base64(*argv);
+        printf("%s\n", hex_to_base64(argv[1]));
     }
 
     return 0;
